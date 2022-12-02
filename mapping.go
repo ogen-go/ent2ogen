8310c19bf7f5e02a70b08fd50b5332e3ab7e9867
rs <shepper116@gmail.com>
2022-12-02 13:04:03 +0500

    feat: support strings type

--- mapping.go
+++ mapping.go
@@ -154,76 +154,145 @@ func (m *Mapping) createFieldMapping(entField *gen.Field, ogenField *ir.Field) e
 		panic("unreachable")
 	}
 
-	type tf struct {
-		Type   jsonschema.SchemaType
-		Format string
-	}
-	mapping := map[field.Type]tf{
-		field.TypeBool:   {jsonschema.Boolean, ""},
-		field.TypeString: {jsonschema.String, ""},
-		field.TypeInt:    {jsonschema.Integer, ""},
-		field.TypeInt32:  {jsonschema.Integer, "int32"},
-		field.TypeInt64:  {jsonschema.Integer, "int64"},
-		field.TypeTime:   {jsonschema.String, "date-time"},
-		field.TypeUUID:   {jsonschema.String, "uuid"},
-		field.TypeEnum:   {jsonschema.String, ""},
-	}
+	var (
+		et = entField.Type
+		js = ogenField.Spec.Schema
+	)
 
-	v, ok := mapping[entField.Type.Type]
-	if !ok {
+	switch entField.Type.Type {
+	case field.TypeBool:
+		if js.Type != jsonschema.Boolean {
+			return fmt.Errorf("type mismatch: expected boolean but have %q", js.Type)
+		}
+
+	case field.TypeString:
+		if js.Type != jsonschema.String {
+			return fmt.Errorf("type mismatch: expected string but have %q", js.Type)
+		}
+
+	case field.TypeInt:
+		if js.Type != jsonschema.Integer {
+			return fmt.Errorf("type mismatch: expected integer but have %q", js.Type)
+		}
+
+	case field.TypeInt32:
+		if js.Type != jsonschema.Integer {
+			return fmt.Errorf("type mismatch: expected integer but have %q", js.Type)
+		}
+		if js.Format != "int32" {
+			return fmt.Errorf("format mismatch: expected int32 but have %q", js.Format)
+		}
+
+	case field.TypeInt64:
+		if js.Type != jsonschema.Integer {
+			return fmt.Errorf("type mismatch: expected integer but have %q", js.Type)
+		}
+		if js.Format != "int64" {
+			return fmt.Errorf("format mismatch: expected int64 but have %q", js.Format)
+		}
+
+	case field.TypeTime:
+		if js.Type != jsonschema.String {
+			return fmt.Errorf("type mismatch: expected string but have %q", js.Type)
+		}
+		if js.Format != "date-time" {
+			return fmt.Errorf("format mismatch: expected date-time but have %q", js.Format)
+		}
+
+	case field.TypeUUID:
+		if js.Type != jsonschema.String {
+			return fmt.Errorf("type mismatch: expected string but have %q", js.Type)
+		}
+		if js.Format != "uuid" {
+			return fmt.Errorf("format mismatch: expected uuid but have %q", js.Format)
+		}
+
+	case field.TypeEnum:
+		if js.Type != jsonschema.String {
+			return fmt.Errorf("type mismatch: expected string but have %q", js.Type)
+		}
+
+	case field.TypeJSON:
+		switch et.Ident {
+		case "[]string":
+			if js.Type != jsonschema.Array {
+				return fmt.Errorf("type mismatch: expected array but have %q", js.Type)
+			}
+			if js.Item == nil {
+				return fmt.Errorf("items field must be specified")
+			}
+			if js.Item.Type != jsonschema.String {
+				return fmt.Errorf("items: expected string but have %q", js.Item.Type)
+			}
+		default:
+			return fmt.Errorf("unsupported ent json type: %s", et.Ident)
+		}
+	default:
 		return fmt.Errorf("unsupported ent type: %s", entField.Type.ConstName())
 	}
 
-	ogenSchema := ogenField.Spec.Schema
-	if ogenSchema.Type != v.Type {
-		return fmt.Errorf("type mismatch: expected %q but have %q", v.Type, ogenSchema.Type)
+	fm := FieldMapping{
+		From: entField,
+		To:   ogenField,
 	}
 
-	if ogenSchema.Format != v.Format {
-		return fmt.Errorf("type format mismatch: expected %q but have %q", v.Format, ogenSchema.Format)
-	}
-
-	var enumMappings []EnumMapping
 	if entField.Type.Type == field.TypeEnum {
-		if len(ogenSchema.Enum) != len(entField.EnumValues()) {
-			return fmt.Errorf("enum mismatch")
+		enumMappings, err := m.createEnumMappings(entField, ogenField)
+		if err != nil {
+			return fmt.Errorf("enum: %w", err)
 		}
 
-		dbEnums := make(map[string]gen.Enum, len(entField.Enums))
-		for _, enum := range entField.Enums {
-			dbEnums[enum.Value] = enum
-		}
-
-		typ := ogenField.Type
-		if typ.IsGeneric() { // Nullable enums.
-			typ = typ.GenericOf
-		}
-
-		for _, ogenEnum := range typ.EnumVariants {
-			val, ok := ogenEnum.Value.(string)
-			if !ok {
-				return fmt.Errorf("unexpected enum value type: %T", ogenEnum.Value)
-			}
-
-			dbEnum, ok := dbEnums[val]
-			if !ok {
-				return fmt.Errorf("enum value %q not found in ent schema", val)
-			}
-
-			enumMappings = append(enumMappings, EnumMapping{
-				From: dbEnum,
-				To:   ogenEnum,
-			})
-		}
+		fm.Enums = enumMappings
 	}
 
-	m.FieldMappings = append(m.FieldMappings, FieldMapping{
-		From:  entField,
-		To:    ogenField,
-		Enums: enumMappings,
-	})
-
+	m.FieldMappings = append(m.FieldMappings, fm)
 	return nil
+}
+
+func (m *Mapping) createEnumMappings(ef *gen.Field, gf *ir.Field) ([]EnumMapping, error) {
+	var (
+		enumMappings []EnumMapping
+
+		et = ef.Type
+		js = gf.Spec.Schema
+	)
+
+	if et.Type != field.TypeEnum || len(js.Enum) == 0 {
+		return nil, fmt.Errorf("bad schema")
+	}
+
+	if len(js.Enum) != len(ef.EnumValues()) {
+		return nil, fmt.Errorf("enum mismatch")
+	}
+
+	dbEnums := make(map[string]gen.Enum, len(ef.Enums))
+	for _, enum := range ef.Enums {
+		dbEnums[enum.Value] = enum
+	}
+
+	typ := gf.Type
+	if typ.IsGeneric() { // Nullable enums.
+		typ = typ.GenericOf
+	}
+
+	for _, ogenEnum := range typ.EnumVariants {
+		val, ok := ogenEnum.Value.(string)
+		if !ok {
+			return nil, fmt.Errorf("unexpected enum value type: %T", ogenEnum.Value)
+		}
+
+		dbEnum, ok := dbEnums[val]
+		if !ok {
+			return nil, fmt.Errorf("enum value %q not found in ent schema", val)
+		}
+
+		enumMappings = append(enumMappings, EnumMapping{
+			From: dbEnum,
+			To:   ogenEnum,
+		})
+	}
+
+	return enumMappings, nil
 }
 
 func (m *Mapping) lookupEdge(name string) (*gen.Edge, bool, error) {
